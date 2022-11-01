7cfccc42bb963b7eb2f38e6fbc1ad4699dcc77bc
Aitana Shough <aeshough@gmail.com>
2022-10-31 21:13:39 -0700

    add basic index HTML and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Business Logic for AddressBook ---------
-
-function AddressBook () {
+function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
@@ -30,15 +29,10 @@ AddressBook.prototype.deleteContact = function(id) {
   return true;
 };
 
-AddressBook.prototype.update = function(id, key, value) {
-  this.contacts[id][key] = value;
-};
-
 // Business Logic for Contacts ---------
-
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
-  this.lastName = lastName; 
+  this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
@@ -46,3 +40,33 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  addressBook.addContact(newContact);
+  console.log(addressBook.contacts);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
